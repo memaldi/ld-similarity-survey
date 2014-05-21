cd79e0798781dcb3bbd610c0ff774e3f62c40811
Mikel Emaldi <m.emaldi@gmail.com>
2014-05-21 12:03:01 +0200

    Implementing tests

--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
-
+from django.test import TestCase, TransactionTestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
 # Create your tests here.
+class RegisterTestCase(TransactionTestCase):
+    def test_register_user(self):
+        self.assertEqual(len(User.objects.all()), 0)
+        c = Client()
+        # Registering a user
+        c.post('/register/', {'user': 'user1', 'password': 'user1', 'password-repeat': 'user1'})
+        self.assertEqual(len(User.objects.all()), 1)
+        # Registering a user with a repeated username
+        c = Client()
+        c.post('/register/', {'user': 'user1', 'password': 'user1', 'password-repeat': 'user1'})
+        self.assertEqual(len(User.objects.all()), 1)
+        # Registering a user wiht missmatched password
+        c = Client()
+        c.post('/register/', {'user': 'user2', 'password': 'user2', 'password-repeat': 'user1'})
+        self.assertEqual(len(User.objects.all()), 1)
+        # Registering another user
+        c = Client()
+        c.post('/register/', {'user': 'user2', 'password': 'user2', 'password-repeat': 'user2'})
+        self.assertEqual(len(User.objects.all()), 2)
