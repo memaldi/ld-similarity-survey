33292c8eafa812b76e518e7119771eab0ba20b71
Mikel Emaldi <m.emaldi@gmail.com>
2014-05-22 10:40:27 +0200

    Minor changes

--- ld_similarity_survey/settings.py
+++ ld_similarity_survey/settings.py
@@ -49,17 +49,6 @@ ROOT_URLCONF = 'ld_similarity_survey.urls'
 
 WSGI_APPLICATION = 'ld_similarity_survey.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
--- travis_settings.py
+++ travis_settings.py
@@ -2,3 +2,13 @@ from ld_similarity_survey.settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'c9--1=a9ya2q$lclnrcy@3imnr)5#-18lyxn^my)v6b+k#rq@a'
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
