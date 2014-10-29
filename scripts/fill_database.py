b34553bcd9b0fc5c134df98eb1d5277926e277f6
Mikel Emaldi <m.emaldi@gmail.com>
2014-10-29 10:49:54 +0100

    New script that gives nicks to datasets and creates similarity objects

--- scripts/fill_database.py
+++ scripts/fill_database.py
@@ -10,35 +10,13 @@ with open('survey_datasets.csv') as f:
     for line in f:
         sline = line.split(',')
         if sline[4] not in ['', '*'] and sline[1] != '':
-            url = sline[1]
-            params = {'id': get_id(url)}
-            r = requests.get('%s/action/package_show' % DATAHUB_API_URL, params=params)
-            json_data = r.json()
-            title = ''
-            description = ''
-            if 'result' in json_data:
-                if 'title' in json_data['result']:
-                    if json_data['result']['title'] != None:
-                        title = json_data['result']['title']
-                if 'notes' in json_data['result']:
-                    if json_data['result']['notes'] != None:
-                        description = json_data['result']['notes']
-                example_resource = ''
-                for resource in json_data['result']['resources']:
-                    if resource['format'] == 'example/rdf+xml':
-                        if resource['url'] != None:
-                            example_resource = resource['url']
-                print 'Saving %s...' % title
-                nick = sline[4]
-                dataset = Dataset()
-                dataset.title = title
-                dataset.description = description
-                dataset.datahub_url = url
-                dataset.example_resource = example_resource
-                dataset.nick = nick
+            try:
+                dataset = Dataset.objects.get(datahub_url=sline[1])
+                dataset.nick = sline[4]
                 dataset.save()
-            else:
-                print json_data
+            except Exception as e:
+                print e
+                print sline[1]
 
 with open('relations.csv') as f:
     for line in f:
